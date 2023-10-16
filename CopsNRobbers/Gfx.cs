6e4ab8c01f2b587befc85da6d45d39c147d6960f
wiggecom <wigge@hotmail.com>
2023-10-16 11:14:10 +0200

    Base created
    Main objects and some gfx

---
+++ CopsNRobbers/Gfx.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopsNRobbers
+{
+    internal class Gfx
+    {
+        public static void DrawBustedSingleCol()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("╔════════════════════════════════════════════════════════════════════════════════════════╗");
+            Console.WriteLine("║ ▓▓▓▓████▓▓▓▓███▓▓▓▓████▓▓▓▓████▓▓▓▓██▓▓                                                ║");
+            Console.WriteLine("║ ██    ░▒░   ░▒░   ░▒░   ░▒░   ░▒░    ██                                                ║");
+            Console.WriteLine("║ ▓▓    ▒▓▒   ▒▓▒   ▒▓▒   ▒▓▒   ▒▓▒    ▓▓                                                ║");
+            Console.WriteLine("║ ██    ▒▓▒   ▒▓▒   ▒▓▒   ▒▓▒   ▒▓▒    ██  █████  ██  ██ █████ ██████ ████ █████    ███  ║");
+            Console.WriteLine("║ ▓▓    ▒▓▒░▓▓███▓▒ ▒▓▒ ▒▓███▓▓░▒▓▒    ▓▓  ██░░██ ██░ ██░██░░░░  ██░░░██░░░██░░██   ███░ ║");
+            Console.WriteLine("║ ██    ▒▓░▓▓▓░▒░   ▒▓▒   ░▒░▓▓▓░▓▒    ██  █████░░██░ ██░ ███    ██░  ███  ██░  █░   █░░ ║");
+            Console.WriteLine("║ ▓▓    ▒▓░▓▓▓███▓▒ ▒▓▒ ▒▓███▓▓▓░▓▒    ▓▓  ██░░██ ██░ ██░   ██   ██░  ██░░ ██░ ██░    ░  ║");
+            Console.WriteLine("║ ██    ▒▓░▓▓▓░▒░   ▒▓▒   ░▒░▓▓▓░▓▒    ██  █████░░ ████░░████░░  ██░  ████ █████░░   █   ║");
+            Console.WriteLine("║ ▓▓    ▒▓░░▓▓███▓▒ ▒▓▒ ▒▓███▓▓░░▓▒    ▓▓   ░░░░░   ░░░░  ░░░░    ░░   ░░░░ ░░░░░     ░  ║");
+            Console.WriteLine("║ ██    ▒▓░   ░▒░   ▒▓▒   ░▒░   ░▓▒    ██                                                ║");
+            Console.WriteLine("║ ▓▓▓▓████▓▓▓▓███▓▓▓▓████▓▓▓▓████▓▓▓▓██▓▓                                                ║");
+            Console.WriteLine("╚════════════════════════════════════════════════════════════════════════════════════════╝");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public static void DrawRobberySingleCol()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("╔════════════════════════════════════════════════════════════════════════════════════════╗");
+            Console.WriteLine("║      ░░░░░░░░░░                                                                        ║");
+            Console.WriteLine("║    ░░░░░░░░░░░░░                                                                       ║");
+            Console.WriteLine("║   ░░░░░░░░░░░░░░░                                                                      ║");
+            Console.WriteLine("║  ░░░░▒▒▒▒▒▒▒▒▒▒░░              █▓    ████   ███  ████  ████  █████ ████  █   █ █ █ █   ║");
+            Console.WriteLine("║  ░░░                         █▓▒░    █░░░█ █ ░░█ █ ░░█ █ ░░█ █░░░░░█░░░█  █ █ ░█░█░█░  ║");
+            Console.WriteLine("║  ░         ▒█▓   ░          █▓▒░     ████ ░█░  █░████ ░█████░███   ████ ░  █ ░ █░█░█░  ║");
+            Console.WriteLine("║  ░░░▒▒          ▒▒░        █▓▒░      █░░░█ █░  █░█ ░░█ █ ░░█ █░░░  █░░░█   █░   ░ ░ ░  ║");
+            Console.WriteLine("║   ░░▒▒▒▒░░░░░░░░░░░░      █▓▒░       █░  █░ ███ ░████ ░████ ░█████ █░  █░  █░  █ █ █   ║");
+            Console.WriteLine("║   ░░░▒▒░▓▒▓▒▓▒▓▒▓       ██▓▒░▒▒       ░   ░  ░░░  ░░░░  ░░░░  ░░░░░ ░   ░   ░   ░ ░ ░  ║");
+            Console.WriteLine("║   ░░ ░▒▒░▓▒▓▒▓░░░      ▓▒▒░░░▒░▒▒                                                      ║");
+            Console.WriteLine("║   ░  ░▒▒▒░░░░▒░         ▒▒▒▒░░░▒░░                                                     ║");
+            Console.WriteLine("║     ░▒▒▒▒▒▒▒░             ▒▒▒▒░░░                                                      ║");
+            Console.WriteLine("╚════════════════════════════════════════════════════════════════════════════════════════╝");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public static void DrawBusted()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("╔════════════════════════════════════════════════════════════════════════════════════════╗");
+            Console.WriteLine("║ ▓▓▓▓████▓▓▓▓███▓▓▓▓████▓▓▓▓████▓▓▓▓██▓▓                                                ║");
+            Console.WriteLine("║ ██    ░▒░   ░▒░   ░▒░   ░▒░   ░▒░    ██                                                ║");
+            Console.WriteLine("║ ▓▓    ▒▓▒   ▒▓▒   ▒▓▒   ▒▓▒   ▒▓▒    ▓▓                                                ║");
+            Console.WriteLine("║ ██    ▒▓▒   ▒▓▒   ▒▓▒   ▒▓▒   ▒▓▒    ██  █████  ██  ██ █████ ██████ ████ █████    ███  ║");
+
+            Console.Write("║ ▓▓    ▒▓▒");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(" ░▓███▓▒ ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("▒▓▒ ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("▒▓███▓░ ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("▒▓▒    ▓▓  ██░░██ ██░ ██░██░░░░  ██░░░██░░░██░░██   ███░ ║");
+
+
+            Console.Write("║ ██    ▒▓░");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("░▓▓░▒░   ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("▒▓▒   ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("░▒░▓▓░");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("░▓▒    ██  █████░░██░ ██░ ███    ██░  ███  ██░  █░   █░░ ║");
+
+
+            Console.Write("║ ▓▓    ▒▓░");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("░▓▓███▓▒ ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("▒▓▒ ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("▒▓███▓▓░");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("░▓▒    ▓▓  ██░░██ ██░ ██░   ██   ██░  ██░░ ██░ ██░    ░  ║");
+
+
+            Console.Write("║ ██    ▒▓░");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("░▓▓░▒░   ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("▒▓▒   ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("░▒░▓▓░");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("░▓▒    ██  █████░░ ████░░████░░  ██░  ████ █████░░   █   ║");
+
+
+            Console.Write("║ ▓▓    ▒▓░");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(" ░▓███▓▒ ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("▒▓▒ ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("▒▓███▓░ ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("░▓▒    ▓▓   ░░░░░   ░░░░  ░░░░    ░░   ░░░░ ░░░░░     ░  ║");
+
+
+            Console.WriteLine("║ ██    ▒▓░   ░▒░   ▒▓▒   ░▒░   ░▓▒    ██                                                ║");
+            Console.WriteLine("║ ▓▓▓▓████▓▓▓▓███▓▓▓▓████▓▓▓▓████▓▓▓▓██▓▓                                                ║");
+            Console.WriteLine("╚════════════════════════════════════════════════════════════════════════════════════════╝");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+        }
+        public static void DrawRobbery()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("╔════════════════════════════════════════════════════════════════════════════════════════╗");
+            Console.WriteLine("║      ░░░░░░░░░░                                                                        ║");
+            Console.WriteLine("║    ░░░░░░░░░░░░░                                                                       ║");
+            Console.WriteLine("║   ░░░░░░░░░░░░░░░                                                                      ║");
+            Console.WriteLine("║  ░░░░▒▒▒▒▒▒▒▒▒▒░░              █▓    ████   ███  ████  ████  █████ ████  █   █ █ █ █   ║");
+            Console.WriteLine("║  ░░░                         █▓▒░    █░░░█ █ ░░█ █ ░░█ █ ░░█ █░░░░░█░░░█  █ █ ░█░█░█░  ║");
+            Console.Write("║  ░         ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("▒█▓");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("   ░          █▓▒░     ████ ░█░  █░████ ░█████░███   ████ ░  █ ░ █░█░█░  ║");
+            Console.WriteLine("║  ░░░▒▒          ▒▒░        █▓▒░      █░░░█ █░  █░█ ░░█ █ ░░█ █░░░  █░░░█   █░   ░ ░ ░  ║");
+            Console.WriteLine("║   ░░▒▒▒▒░░░░░░░░░░░░      █▓▒░       █░  █░ ███ ░████ ░████ ░█████ █░  █░  █░  █ █ █   ║");
+            Console.Write("║   ░░░▒▒░");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("▓▒▓▒▓▒▓▒▓");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("       ██▓▒░▒▒       ░   ░  ░░░  ░░░░  ░░░░  ░░░░░ ░   ░   ░   ░ ░ ░  ║");
+            Console.Write("║   ░░ ░▒▒░");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("▓▒▓▒▓");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("░░░      ▓▒▒░░░▒░▒▒                                                      ║");
+            Console.WriteLine("║   ░  ░▒▒▒░░░░▒░         ▒▒▒▒░░░▒░░                                                     ║");
+            Console.WriteLine("║     ░▒▒▒▒▒▒▒░             ▒▒▒▒░░░                                                      ║");
+            Console.WriteLine("╚════════════════════════════════════════════════════════════════════════════════════════╝");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+        }
+    }
+}
---
+++ CopsNRobbers/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CopsNRobbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) // Gfx display loop
+            {
+                // Single color graphics
+            Gfx.DrawRobberySingleCol();
+            Console.WriteLine();
+            Gfx.DrawBustedSingleCol();
+            Console.WriteLine();
+            Console.ReadKey();
+
+                // Dual color graphics
+            Console.Clear();
+            Gfx.DrawRobbery();
+            Console.WriteLine();
+            Gfx.DrawBusted();
+            Console.WriteLine();
+            Console.ReadKey();
+            }
+        }
+    }
+}
